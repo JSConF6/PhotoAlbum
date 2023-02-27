66f0f1690a686db40fbe4d84f42a1cd2de93028a
JSConF <arare30030@naver.com>
2023-02-27 15:33:25 +0900

    Album Domain 생성

---
+++ src/main/java/com/squarecross/photoalbum/domain/Album.java
@@ -0,0 +1,50 @@
+package com.squarecross.photoalbum.domain;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name="album", schema = "photo_album", uniqueConstraints = {@UniqueConstraint(columnNames = "album_id")}) // name: 테이블명, schema: 스키마명, uniqueConstraints: 반복되면 안되는 제약조건
+public class Album {
+
+    @Id // 해당 Entity의 Primary Key로 사용한다는 의미
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // @Id 값을 새롭게 부여할 때 사용하는 방법에 대한 정보를 입력 strategy = GenerationType.IDENTITY 가장 최근 id에 +1을 해서 다음 아이디를 생성
+    @Column(name = "album_id", unique = true, nullable = false) // album 테이블의 매핑되는 column 정보를 입력합니다. name: Column명, unique: 중복불가, nullable: null값 허용
+    private Long albumId;
+
+    @Column(name = "album_name", unique = false, nullable = false)
+    private String albumName;
+
+    @Column(name = "created_at", unique = false, nullable = true)
+    @CreatedDate // 새로운 앨범을 생성해 DB INSERT할 때 자동으로 현재 시간을 입력해줍니다.
+    private Date createdAt;
+
+    public Album() {
+    }
+
+    public Long getAlbumId() {
+        return albumId;
+    }
+
+    public void setAlbumId(Long albumId) {
+        this.albumId = albumId;
+    }
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+    public void setAlbumName(String albumName) {
+        this.albumName = albumName;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}
