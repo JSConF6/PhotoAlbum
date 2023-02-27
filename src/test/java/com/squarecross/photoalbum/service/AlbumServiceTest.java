f6b8564b5183cd7bb94bf5acb35112311b38565d
JSConF <arare30030@naver.com>
2023-02-27 16:07:02 +0900

    AlbumService Test 클래스 생성 및 getAlbum 테스트 성공

---
+++ src/test/java/com/squarecross/photoalbum/service/AlbumServiceTest.java
@@ -0,0 +1,31 @@
+package com.squarecross.photoalbum.service;
+
+import com.squarecross.photoalbum.domain.Album;
+import com.squarecross.photoalbum.repository.AlbumRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest // 스프링 컴테이너 내에 있는 모든 빈을 DI로 가져와서 사용할 수 있도록 만듭니다.
+@Transactional // 테스트로 생성한 데이터는 실제로 DB에 반영되지 않도록 Commit을 하지 않는다.
+class AlbumServiceTest {
+
+    @Autowired
+    AlbumRepository albumRepository;
+
+    @Autowired
+    AlbumService albumService;
+
+    @Test
+    void getAlbum() {
+        Album album = new Album();
+        album.setAlbumName("테스트");
+        Album savedAlbum = albumRepository.save(album);
+
+        Album resAlbum = albumService.getAlbum(savedAlbum.getAlbumId());
+        assertEquals("테스트", resAlbum.getAlbumName());
+    }
+}
